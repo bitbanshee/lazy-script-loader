ec24c8090b3236df0c5f2a242d66fef9566fc238
George Melgaço <george.melgaco@bwtech.com>
2018-01-16 16:34:24 -0200

    I don't know how this was working before D:

--- README.md
+++ README.md
@@ -5,14 +5,38 @@
 __Lazy Script Loader__ works using a `info` object, as described below:
 ```js
 var infoEx = {
-    // Function name
+    // Component name
     name: `Cat`,
     // URL
     url: `scripts/animals/Cat.js`
 }
 ```
 
+## Initialization
+You must provide an object containing at least a global environment. You can provide a local environment and a URL prefix string.
+```js
+// Configuration object to be passed
+{
+    // Global environment (mandatory)
+    environment: window//,
+    // Local environment (optional)
+    //localEnvironment: window,
+    // URL prefix (optional)
+	//scriptURLPrefix: 'scripts/'
+}
+```
+
 ## Methods
 ### `importScript(info)` -> `Promise`
-* `info`: an [info object](#info-object) describing a function to be loaded. A Promise resolved with the function is returned.
-First, `info.name` is checked and the request to the `info.url` is made if the function is not loaded yet.
+* `info`: an [info object](#info-object) describing a script to be loaded or a string representing an element name if a URL prefix was provided.
+
+A Promise resolved with the element is returned. First, `info.name` is checked and the request to the `info.url` is made if the element is not loaded yet.
+
+### `importScripts(infos)` -> `Promise`
+* `infos`: an array of [info objects](#info-object) describing a script to be loaded or a string representing an element name if a URL prefix was provided.
+
+`importScript` is called internaly for each info object in the array and a `Promise.all` of the generated promises is returned.
+
+### `importTo(localEnvironment, info)` -> `Promise`
+* `localEnvironment`: an object to which the loaded element will be assigned.
+* `info`: an [info object](#info-object) describing a script to be loaded or a string representing an element name if a URL prefix was provided.
--- lazy-script-loader.js
+++ lazy-script-loader.js
@@ -1,10 +1,10 @@
-;(function (environment) {
-	Object.assign(environment, init());
+;(function ({ environment, localEnvironment, scriptURLPrefix }) {
+	return init();
 
 	function init() {
 		const lazyLoader = {
-			// Used as a default prefix for URLs when calling `importScript` using string as param
-			defaultURLPrefix: '',
+			// Used as a default prefix for URLs when calling `importScript`
+			scriptURLPrefix: scriptURLPrefix || '',
 			/**
 			 * (Object|string) -> Promise
 			 * @param {Object|string} info
@@ -12,14 +12,21 @@
 			 * @param {String} info.url
 			 * @return {PromiseLike<object>}
 			 */
-			importScript: lazyLoad_,
+			importScript: leftPartialApply(lazyLoad_, localEnvironment),
 			/**
 			 * Array<object|string> -> Array<PromiseLike<object>>
 			 * @param {Array<{ name: string, url: string }|string>}
 			 * @return {Array<PromiseLike<object>>}
 			 */
 			importScripts: function (infos) {
-				return infos.map(lazyLoad_);
+				return Promise.all(infos.map(leftPartialApply(lazyLoad_, localEnvironment)))
+			},
+			importTo: function (localEnvironment, info) {
+				const loader = leftPartialApply(lazyLoad_, localEnvironment);
+				if (Array.isArray(info)) {
+					return Promise.all(info.map(loader));
+				}
+				return loader(info);
 			}
 		};
 
@@ -32,16 +39,19 @@
 		 * @param {String} info.url
 		 * @return {Function}
 		 */
-		function promiseLoad_(info) {
+		function promiseLoad_(localEnvironment, info) {
 			return (resolve, reject) => {
 				let head = document.getElementsByTagName('head')[0],
 					script = document.createElement('script');
 				script.type = 'text/javascript';
-				script.src = info.url;
+				script.src = info.url.startsWith(lazyLoader.scriptURLPrefix) ?
+					info.url :
+					lazyLoader.scriptURLPrefix + info.url;
 
-				script.onload = function () {
+				script.onload = function (e) {
 					head.removeChild(script);
-					resolve(environment[info.name]);
+					const loadedEntity = decideEnvironment(localEnvironment, info.name);
+					resolve(loadedEntity);
 				};
 
 				script.onerror = function () {
@@ -59,19 +69,152 @@
 		 * @param {String} info.url
 		 * @return {PromiseLike<object>}
 		 */
-		function lazyLoad_(info) {
+		function lazyLoad_(localEnvironment, info, mustAvoidCache = true) {
 			if (typeof info == 'string') {
 				info = {
 					name: info,
-					url: lazyLoader.defaultURLPrefix + info + '.js'
+					url: buildURL(info, mustAvoidCache)
+				}
+			} else {
+				info.url = resolveURL(info.url, mustAvoidCache);
+			}
+
+			const entity = decideEnvironment(localEnvironment, info.name);
+			if (entity) {
+				return Promise.resolve(entity);
+			}
+
+			return new Promise(promiseLoad_(localEnvironment, info));
+
+			function buildURL (scriptName, mustAvoidCache = true) {
+				return resolveURL(lazyLoader.scriptURLPrefix + scriptName + '.js');
+			}
+
+			function resolveURL (url, mustAvoidCache = true) {
+				return url + (mustAvoidCache === true ? '?avoidCache=' + ~~(Math.random() * 1000) : '');
+			}
+		}
+
+		function decideEnvironment (localEnvironment, key) {
+			if (localEnvironment) {
+				if (!localEnvironment[key] && environment[key]) {
+					/**
+					 * One could argue clone deep is unnecessary, but it's
+					 * a way to isolate contexts 
+					 */
+					localEnvironment[key] = cloneDeep(environment[key]);
+				}
+				
+				if (localEnvironment[key]) {
+					return localEnvironment[key];
 				}
 			}
+			return environment[key];
+		}
 
-			if (environment[info.name]) {
-				return Promise.resolve(environment[info.name]);
+		/**
+		 * @param {Function} fn 
+		 * @param {Array<any>} args 
+		 * @return {Function}
+		 */
+		function leftPartialApply (fn, ...args) {
+			return fn.bind(null, ...args);
+		}
+
+		function clone (object) {
+			return leftPartialApply(_clone, buildNewDescriptors)(object);
+		}
+	
+		function cloneDeep (object) {
+			return leftPartialApply(_clone, buildNewDescriptorsDeep)(object);
+		}
+	
+		/**
+		 * @param {Function} buildDescriptorsFn
+		 * @param {object} object
+		 * @return {object}
+		 */
+		function _clone (buildDescriptorsFn, object) {
+			if (!(object instanceof Object)) {
+				return object;
 			}
+		
+			const
+				descriptors = Object.getOwnPropertyDescriptors(object),
+				descriptorsEntries = Object.entries(descriptors),
+				newDescriptors = buildDescriptorsFn(descriptorsEntries);
+			
+			let clone = {};
+			if (object instanceof Function) {
+				clone = function () {
+					return object.apply(this, arguments);
+				};
+			}
+			return Object.defineProperties(clone, newDescriptors);
+		}
 
-			return new Promise(promiseLoad_(info));
+		/**
+		 * @param {Array<Array<string, object>>} descriptorsEntries
+		 * @return {object}
+		 */
+		function buildNewDescriptors (descriptorsEntries) {
+			return descriptorsEntries.reduce(entriesToCloned, {});
+		}
+
+		/**
+		 * @param {Array<Array<string, object>>} descriptorsEntries
+		 * @return {object}
+		 */
+		function buildNewDescriptorsDeep (descriptorsEntries) {
+			const
+				valuedDescriptorsEntries = descriptorsEntries.filter(([key, value]) => value.value != null),
+				lazyValuedDescriptorsEntries = descriptorsEntries.filter(([key, value]) => !!value.get),
+				newValuedDescriptors = valuedDescriptorsEntries.reduce(entriesToClonedDeep, {}),
+				newLazyValuedDescriptors = lazyValuedDescriptorsEntries.reduce(entriesToCloned, {});
+
+			return Object.assign({}, newValuedDescriptors, newLazyValuedDescriptors);
+		}
+
+		/**
+		 * @param {object} descriptors 
+		 * @param {Array<string, object>} param1 
+		 * @return {object}
+		 */
+		function entriesToClonedDeep (descriptors, [key, descriptor]) {
+			return Object.defineProperty(
+				descriptors,
+				key,
+				{
+					enumerable: true,
+					value: Object.assign(
+						{},
+						descriptor,
+						{ value: clone(descriptor.value) }
+					)
+				}
+			);
+		}
+
+		/**
+		 * @param {object} descriptors 
+		 * @param {Array<string, object>} param1 
+		 * @return {object}
+		 */
+		function entriesToCloned (descriptors, [key, descriptor]) {
+			return Object.defineProperty(
+				descriptors,
+				key,
+				{
+					enumerable: true,
+					value: Object.assign(
+						{},
+						descriptor
+					)
+				}
+			);
 		}
 	}
-})(window);
+})({
+	environment: window,
+	scriptURLPrefix: 'scripts/'
+});
